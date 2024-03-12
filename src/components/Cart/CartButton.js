9492441fc2f3ee1bd670d92a2714a42dc095dec0
MTahir01 <tahirkadgaonkar01@gmail.com>
2024-03-12 12:56:37 +0530

    cart open/close func

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,8 +1,16 @@
-import classes from './CartButton.module.css';
+import classes from "./CartButton.module.css";
+import { uiActions } from "../../store/ui-slice";
+import { useDispatch, useSelector } from "react-redux";
 
 const CartButton = (props) => {
+  const dispatch = useDispatch();
+  const cartIsVisible = useSelector((state) => state.ui.cartIsVisible);
+  const toggleCartHandler = (e) => {
+    e.preventDefault();
+    dispatch(uiActions.toggle());
+  };
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={toggleCartHandler}>
       <span>My Cart</span>
       <span className={classes.badge}>1</span>
     </button>
