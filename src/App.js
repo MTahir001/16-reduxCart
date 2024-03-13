24e9538b6a01ccec60b6fc8a0ac87f8544912acc
MTahir01 <tahirkadgaonkar01@gmail.com>
2024-03-13 13:05:34 +0530

    PUT data in database via useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import { useEffect } from "react";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import { useSelector } from "react-redux";
 function App() {
   const cartIsVisible = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
 
+  useEffect(() => {
+    fetch("https://reactredux-f7d77-default-rtdb.firebaseio.com/cart.json", {
+      method: "PUT",
+      body: JSON.stringify(cart),
+    });
+  }, [cart]);
   return (
     <Layout>
       {cartIsVisible && <Cart />}
