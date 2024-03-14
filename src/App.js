d8ba431392c089c04e3527a663db3b5de510425c
MTahir01 <tahirkadgaonkar01@gmail.com>
2024-03-14 08:30:50 +0530

    showing Notification bar on different status, compo-errorHandling-etc

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import { useEffect } from "react";
+import { Fragment, useEffect } from "react";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { uiActions } from "./store/ui-slice";
+import Notification from "./components/UI/Notification";
+
+let isInitial = true;
 function App() {
+  const dispatch = useDispatch();
   const cartIsVisible = useSelector((state) => state.ui.cartIsVisible);
   const cart = useSelector((state) => state.cart);
+  const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
-    fetch("https://reactredux-f7d77-default-rtdb.firebaseio.com/cart.json", {
-      method: "PUT",
-      body: JSON.stringify(cart),
+    const sendCartData = async () => {
+      dispatch(
+        uiActions.showNotification({
+          status: "pending",
+          title: "sending...",
+          message: "Sending cart data...",
+        })
+      );
+      const response = await fetch(
+        "https://reactredux-f7d77-default-rtdb.firebaseio.com/car",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Failed to send data!!!");
+      }
+
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success!",
+          message: "Cart data sent successfully!",
+        })
+      );
+    };
+
+    if (isInitial) {
+      // first time we don't want to send cart
+      // cart is empty, overrides old data
+      isInitial = false;
+      return;
+    }
+    sendCartData().catch((error) => {
+      if (error.message.includes("Failed to fetch")) {
+        return dispatch(
+          uiActions.showNotification({
+            status: "error",
+            title: "Error❌",
+            message: "Failed to fetch data. Please check your URL.",
+          })
+        );
+      }
     });
-  }, [cart]);
+  }, [cart, dispatch]);
   return (
-    <Layout>
-      {cartIsVisible && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      {notification && <Notification {...notification} />}
+      <Layout>
+        {cartIsVisible && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
